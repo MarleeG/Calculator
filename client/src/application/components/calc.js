f8df961552eb99dd9a07f9ffe48d58c428e5614a
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-05-26 22:45:06 -0400

    console logging clicks in dev console.

--- client/src/application/components/calc.js
+++ client/src/application/components/calc.js
@@ -1,50 +1,56 @@
-import React, {useEffect, useState} from "react";
-import {numbers, operations } from '../../shared/data/data';
-
+import React, { useEffect, useState } from "react";
+import { numbers, operations } from "../../shared/data/data";
 
 import "./calc.css";
 import Button from "../../shared/UIElements/Button";
 
 const Calc = (props) => {
-    const numStyle = {
-        width: "7vw",
-        height: "7vw",
-        borderRadius:"20px",
-        margin: "10px"
-    };
+  const numStyle = {
+    width: "7vw",
+    height: "7vw",
+    borderRadius: "20px",
+    margin: "10px",
+  };
 
-    const operationStyles= {
-        width: "7vw",
-        height: "12.9vw",
-        borderRadius:"10px",
-        
-    }
+  const operationStyles = {
+    width: "7vw",
+    height: "12.9vw",
+    borderRadius: "10px",
+  };
 
+  const [allNums, setAllNum] = useState([]);
+  const [allOperations, setAllOperations] = useState([]);
+  const [selectedBtns, setSelectedBtns]= useState([]);
 
-    const [allNums, setAllNum] = useState([]);
-    const [allOperations, setAllOperations] = useState([])
+  const updateSelectedBtn = (btnVal) => {
+      setSelectedBtns([...selectedBtns, btnVal]);
+  };
 
-    useEffect(() => {
-        setAllNum(numbers());
-        setAllOperations(operations());
-    }, []);
+  useEffect(() => {
+    setAllNum(numbers());
+    setAllOperations(operations());
+    console.log(selectedBtns);
+
+  }, [selectedBtns]);
+
   return (
     <div className="calc__container">
       <div className="calc__wrapper">
-        <div className="calc__input-wrap">{/* STATIC INPUT */} Static input</div>
+        <div className="calc__input-wrap">
+          {/* STATIC INPUT */} Static input
+        </div>
         <div className="calc__buttons-wrap">
           <div className="calc__col calc__col-one">
             {/* numbers */}
             {allNums.map((num) => {
-                return <Button key={num} styles={numStyle} value={num}/>
+              return <Button key={num} styles={numStyle} value={num} updateSelectedBtn={updateSelectedBtn}/>;
             })}
-
           </div>
 
           <div className="calc__col calc__col-two">
             {/* operations */}
             {allOperations.map((op, idx) => {
-                return <Button key={idx} styles={operationStyles} value={op}/>
+              return <Button key={idx} styles={operationStyles} value={op} updateSelectedBtn={updateSelectedBtn}/>;
             })}
           </div>
         </div>
