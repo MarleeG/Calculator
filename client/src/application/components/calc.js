0f77a6c799b4a04b553d3e06a75a8516f634ccca
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-05-26 22:26:16 -0400

    initial buttons have been created

--- client/src/application/components/calc.js
+++ client/src/application/components/calc.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {numbers, operations } from '../../shared/data/data';
+
 
 import "./calc.css";
+import Button from "../../shared/UIElements/Button";
 
 const Calc = (props) => {
+    const numStyle = {
+        width: "7vw",
+        height: "7vw",
+        borderRadius:"20px",
+        margin: "10px"
+    };
+
+    const operationStyles= {
+        width: "7vw",
+        height: "12.9vw",
+        borderRadius:"10px",
+        
+    }
+
+
+    const [allNums, setAllNum] = useState([]);
+    const [allOperations, setAllOperations] = useState([])
+
+    useEffect(() => {
+        setAllNum(numbers());
+        setAllOperations(operations());
+    }, []);
   return (
     <div className="calc__container">
       <div className="calc__wrapper">
@@ -10,12 +35,17 @@ const Calc = (props) => {
         <div className="calc__buttons-wrap">
           <div className="calc__col calc__col-one">
             {/* numbers */}
-            numbers
+            {allNums.map((num) => {
+                return <Button key={num} styles={numStyle} value={num}/>
+            })}
+
           </div>
 
           <div className="calc__col calc__col-two">
             {/* operations */}
-            operations
+            {allOperations.map((op, idx) => {
+                return <Button key={idx} styles={operationStyles} value={op}/>
+            })}
           </div>
         </div>
       </div>
