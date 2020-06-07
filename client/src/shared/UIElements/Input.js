54055591ba42bcab94bea5a7c110959628fd7859
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-06-07 19:01:49 -0400

    button values now display on input

---
+++ client/src/shared/UIElements/Input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Input.css';
+
+const Input = props => {
+    return  <input type="text" value={props.value} name="selection" ref={props.ref} onChange={props.onChange} disabled={props.disabled ? true : false}/>
+}
+
+export default Input;
